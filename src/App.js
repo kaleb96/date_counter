69d6624caa36e87b8b2a6c871b073d36533f10e3
KiYoonPark <yes505304@ipactory.com>
2025-02-19 18:40:44 +0900

    create: basic date picker

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
-import "./App.css";
+import { useState, useEffect } from "react";
+import { format } from "date-fns";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 function App() {
   return (
-    <div>
-      <h1>Hello World!</h1>
+    <div className="container">
+      <PickDate />
     </div>
   );
 }
 
+// Datepicker Component
+function PickDate() {
+  const [date, setDate] = useState(new Date());
+  const [formattedDate, setFormattedDate] = useState("");
+
+  useEffect(() => {
+    const formatted = format(date, "PPP");
+    setFormattedDate(formatted);
+  }, [date]);
+
+  return (
+    <div>
+      <DatePicker
+        inline={true}
+        selected={date}
+        onChange={(date) => setDate(date)}
+        autoFocus={true}
+        dateFormat={"date"}
+      />
+      <h2>{`${formattedDate}`}</h2>
+    </div>
+  );
+}
 export default App;
